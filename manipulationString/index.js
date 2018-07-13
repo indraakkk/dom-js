d56b9d0690fbda49c012491757638c5d3f8c2fe8
Indra Putra <indrakoslab@gmail.com>
2018-07-13 16:35:12 +0700

    add function lower case and uppercase

--- manipulationString/index.js
+++ manipulationString/index.js
@@ -2,19 +2,41 @@ console.log("//=== Manipulation String ===//");
 
 const output = document.getElementById("string-output-paragraph");
 const clickResult = document.getElementById("string-button");
+const lowResult = document.getElementById("low-button");
+const upResult = document.getElementById("up-button");
 
 const getString = () => {
     const stringFirst = document.getElementById("string-first").value;
     const stringSecond = document.getElementById("string-second").value;
-
-    console.log("stringFirst: ", stringFirst);
-    console.log("stringSecond: ", stringSecond);
     
     output.innerText = `First Word: ${stringFirst}
     Second Word: ${stringSecond}`
 }
 
+const caseLow = () => {
+    const stringFirst = document.getElementById("string-first").value;
+    const stringSecond = document.getElementById("string-second").value;
+    
+    const lowFirst = stringFirst.toLowerCase();
+    const lowSec = stringSecond.toLowerCase();
+
+    output.innerText = `First Word: ${lowFirst}
+    Second Word: ${lowSec}`
+}
+
+const caseUp = () => {
+    const stringFirst = document.getElementById("string-first").value;
+    const stringSecond = document.getElementById("string-second").value;
+    
+    const upFirst = stringFirst.toUpperCase();
+    const upSec = stringSecond.toUpperCase();
+
+    output.innerText = `First Word: ${upFirst}
+    Second Word: ${upSec}`
+}
 // clickResult.onclick = getString;
 
 clickResult.addEventListener('click', getString);
+lowResult.addEventListener('click', caseLow);
+upResult.addEventListener('click', caseUp);
 // clickResult.addEventListener('mouseover', getString);
